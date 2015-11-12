a4862d5701fbb26d477493efa6d29470783c6c77
Chelsea Parkkila <cmparkkila@gmail.com>
2015-11-12 00:35:45 -0800

    first commit

---
+++ lab11.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  /*$('button').click(function(){
+    $('div').slideToggle('slow');
+  });*/
+  
+  $('li').hover(function(){
+    $(this).fadeTo(300, 0.25);
+  }, function(){
+     $(this).fadeTo(300, 1);
+
+  });
+});
+
+$(function(){
+   $('li').click(function () {
+      $(this).text(function(i, text){
+          return text === "CLICKY!" ? "" : "CLICKY!";
+      })
+   });
+})
